075ffb7abd121f89f9632e730e85c1a1768bc400
Tran Nguyen <tran.nguyen@atcgov.onmicrosoft.com>
2023-07-18 15:25:50 -0700

    bad commit 7/18 (fixed)

---
+++ copilotTest.js
@@ -0,0 +1,7 @@
+function copilotTest() {
+    // write a regex that matches the word "copilot" or "co-pilot"
+
+    // write a function that splits a string into two equal halves
+
+    // write a function that calculates the fps given the number of frames, the start time, and the end time
+}
