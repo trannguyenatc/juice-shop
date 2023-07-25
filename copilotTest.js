85c576c7c5538ab64a0b245eb696e1e49f0b17ab
Tran Nguyen <tran.nguyen@atcgov.onmicrosoft.com>
2023-07-25 11:49:17 -0700

    actions secret

--- copilotTest.js
+++
@@ -1,7 +0,0 @@
-function copilotTest() {
-    // write a regex that matches the word "copilot" or "co-pilot"
-
-    // write a function that splits a string into two equal halves
-
-    // write a function that calculates the fps given the number of frames, the start time, and the end time
-}
